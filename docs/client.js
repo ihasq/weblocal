3a962ee4edece936bd4e7eef1341383ed96f6599
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-02-07 02:25:53 +0900

    wip

--- docs/client.js
+++ docs/client.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register("./sw.js");
+navigator.serviceWorker.register("./sw.js", { type: "module" });
 
 Promise.all([navigator.serviceWorker.ready, new Promise(r_msg => globalThis.onmessage = r_msg)]).then(([{ active: sw }, { data: port }]) => {
 	sw.postMessage(port, [port]);
