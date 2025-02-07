fa3c8bac4a99555d874010a3ffdcbd354cf978f9
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-02-08 00:34:06 +0900

    wip

--- docs/local/sw.js
+++ docs/local/sw.js
@@ -1,10 +1,24 @@
-const handleFetch = async ({ request }) => {
-	const { pathname, searchParams } = new URL(request.url)
-	switch(pathname) {
-		case "/local/": {
-			return new Response("")
+self.onmessage = ({ data: { code, id, data }, target }) => {
+	switch(code) {
+		case "INSTALL": {
+			target.navigate("https://weblocal.dev/local")
+			break;
+		}
+		case "CONNECT": {
+
 		}
 	}
 }
 
-self.onfetch = e => e.respondWith(handleFetch(e))
+const local = new Blob([`<title>connect</title><script type="module">if(window !== parent) (await navigator.serviceWorker.ready).active.postMessage({ code: "CONNECT" });</script>`], { type: "text/html" });
+
+const handleFetch = async ({ request }) => {
+	const { pathname, searchParams } = new URL(request.url);
+	switch(pathname) {
+		case "/local": {
+			return new Response(local)
+		}
+	}
+}
+
+self.addEventListener("fetch", e => e.respondWith(handleFetch), true);
--- pkg/local.ts
+++ pkg/local.ts
@@ -1,12 +1,23 @@
-await new Promise(r_load => document.head.append(
+const { target: loader }: { target: HTMLIFrameElement } = await new Promise(r_load => document.head.append(
 	Object.assign(
 		document.createElement("iframe"),
 		{
-			src: `https://weblocal.dev/local?hostname=${encodeURIComponent(location.hostname)}`,
+			src: `https://weblocal.dev/local?type=load&hostname=${encodeURIComponent(location.hostname)}`,
 			onload: r_load
 
 		}
 	)
 ));
+
+if(loader.contentDocument?.title == "load") {
+	await new Promise(r_ready => Object.assign(loader, {
+		src: `https://weblocal.dev/local?hostname=${encodeURIComponent(location.hostname)}`,
+		onload: r_ready
+	}))
+}
+
+if(loader.contentDocument?.title == "connect") {
+
+}
 
 export { localPort };
