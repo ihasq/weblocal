2bf2ebb1441097895c00e17387202cb1784109d8
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-02-09 16:03:45 +0900

    wip

--- docs/new/establisher-sw.js
+++ docs/new/establisher-sw.js
@@ -1,11 +1,13 @@
+const serverIdMap = {};
+
 self.onmessage = ({ data, source }) => {
-	
+	target.postMessage([]);
 	source.postMessage({  })
 }
 
 self.onfetch = e => e.respondWith(handleFetch(e));
 
-const handleFetch = async ({ request }) => {
+const handleFetch = async ({ request, target }) => {
 	if(request.headers.get("sec-fetch-dest") != "iframe") return new Response("", { status: 502 });
 	const { addr, type } = Object.fromEntries(new URL(request.url).searchParams.entries());
 }
