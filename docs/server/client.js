04944bc48cc915fcf6f29d7d0a114bc56d031c21
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-02-09 15:42:16 +0900

    wip

--- docs/server/client.js
+++ docs/server/client.js
@@ -3,25 +3,27 @@ const [address] = location.hostname.split(".");
 const { target: connector } = await new Promise(r_connector => document.head.append(Object.assign(
 	document.createElement("iframe"),
 	{
-		src: `https://weblocal.dev/local?addr=${encodeURI(address)}`,
+		src: `https://weblocal.dev/local?addr=${encodeURI(address)}&type=${window == parent ? "document" : "frame"}`,
 		onload: r_connector
 	}
 )));
 
-if(connector.contentWindow.title == "connect") {
+navigator.serviceWorker.register("./sw.js");
 
-	navigator.serviceWorker.register("./sw.js");
-	
-	const [{ active: sw }, { data: port }] = await Promise.all([
-	
-		navigator.serviceWorker.ready,
-		new Promise(r_port => self.onmessage = r_port)
-	
-	]);
-	
-	sw.postMessage(port, [port]);
-	
-	open("./", "self");
-} else {
-	console.log("nah")
+const [{ active: sw }, port] = await Promise.all([
+
+	navigator.serviceWorker.ready,
+	new Promise(r_port => self.onmessage = ({ data, target }) => target === connector.contentWindow ? r_port(data) : 0)
+
+])
+
+sw.onmessage = ({ data: { code } }) => {
+	switch(code) {
+		case "OK": {
+			open("./", "self");
+			break;
+		}
+	}
 }
+
+sw.postMessage(port, [port]);
--- pkg/serve.ts
+++ pkg/serve.ts
@@ -2,8 +2,10 @@ import { ESTABLISHER_ORIGIN, ADDRESS_ORIGIN } from "./var";
 import { rand } from "./lib/math";
 
 const
+	tEnc = new TextEncoder(), tDec = new TextDecoder(),
+
 	{ publicKey, privateKey: serverVerifierKey } = await crypto.subtle.generateKey({ name: "ECDSA", namedCurve: "P-521" }, true, ["sign", "verify"]),
-	tDec = new TextDecoder(),
+
 	encodedPublicKey = encodeURI(tDec.decode(await crypto.subtle.exportKey("raw", publicKey))),
 
 	{ target: serverEstablisherFrame }: Event = await new Promise(r_load => document.head.append(
@@ -31,83 +33,86 @@ const
 	),
 
 	establishmentMsgMap: { [key: string]: Function } = {},
-	establishServer = (address: string, encodedAddress: Uint8Array, signature: ArrayBuffer): Promise<MessagePort> => {
+	establishServer = (address: string, encodedAddress: Uint8Array, signature: ArrayBuffer): Promise<[MessagePort, MessagePort]> => {
 		let msgId;
 		while((msgId = rand()) in establishmentMsgMap){};
-		serverEstablisherPort.postMessage({ address, encodedAddress, signature }, [encodedAddress.buffer, signature]);
+		serverEstablisherPort.postMessage([msgId, address, encodedAddress, signature], [encodedAddress.buffer, signature]);
 		return new Promise(r_void => establishmentMsgMap[msgId] = r_void);
-	}
-;
+	},
 
-serverEstablisherPort.onmessage = ({ data: { msgId, serverPort } }) => establishmentMsgMap[msgId]?.(serverPort);
+	registrationMap = {},
 
-const registrationMap = {};
+	serve: WLServe = async (
+		
+		handler,
+		options
 
-const tEnc = new TextEncoder();
+	): Promise<WLServerHandler | undefined> => {
 
-export const serve: WLServe = async (
+		if(!handler) return;
+
+		const serverDriver = Object.assign({
+
+			name: "local",
+			origin: ADDRESS_ORIGIN,
+
+		}, typeof handler == "function" ? Object.assign({ handler }, options) : handler);
+
+
+		let address;
+
+		if((address = serverDriver.name) in registrationMap) while((address = `${serverDriver.name}-${rand()}`) in registrationMap){};
+
+		const
+			encodedAddress = tEnc.encode(address),
+			origin = serverDriver.origin(`${address}-${serverId}`),
+			signature = await crypto.subtle.sign("ECDSA", serverVerifierKey, tEnc.encode(address)),
+
+			serverCallback = async ({ data: { code, id, data }, source }: MessageEvent) => {
+
+				switch(code) {
 	
-	handler,
-	options
+					case "REQUEST": {
+	
+						const
+							[body, bodyUsed, cache, credentials, destination, duplex, headers, integrity, isHistoryNavigation, keepalive, method, mode, redirect, referrer, referrerPolicy, targetAddressSpace, url] = data,
+							response = await serverDriver.handler(new Request(url, { body, cache, credentials, headers, integrity, keepalive, method, redirect, referrer, referrerPolicy })),
+							{ status, statusText } = response
+						;
+	
+						const responseBody = await response.arrayBuffer();
+	
+						(source as MessagePort).postMessage({ code: "RESPONSE", id, data: [responseBody, status, statusText, Object.fromEntries(response.headers.entries())] }, [responseBody])
+	
+						break;
+					}
+				}
+			}
+		;
 
-): Promise<WLServerHandler | undefined> => {
+		(await establishServer(address, encodedAddress, signature)).forEach(serverPort => serverPort.onmessage = serverCallback);
 
-	if(!handler) return;
+		// await new Promise(r_init => {
 
-	const serverDriver = Object.assign({
+		// 	const { port1: signalPort, port2: signalDest } = new MessageChannel();
 
-		name: "local",
-		origin: ADDRESS_ORIGIN,
+		// 	const { port1: serverPort, port2: serverDest } = new MessageChannel();
 
-	}, typeof handler == "function" ? Object.assign({ handler }, options) : handler);
+		// })
 
+		return {
+			url: origin,
+			async reload() {
 
-	let address;
+			},
+			async close() {
 
-	if((address = serverDriver.name) in registrationMap) while((address = `${serverDriver.name}-${rand()}`) in registrationMap){};
-
-	const
-		encodedAddress = tEnc.encode(address),
-		origin = serverDriver.origin(`${address}-${serverId}`),
-		signature = await crypto.subtle.sign("ECDSA", serverVerifierKey, tEnc.encode(address)),
-		serverDest = await establishServer(address, encodedAddress, signature)
-	;
-
-	serverDest.onmessage = async ({ data: { code, id, data }, source }) => {
-
-		switch(code) {
-
-			case "REQUEST": {
-
-				const
-					[body, bodyUsed, cache, credentials, destination, duplex, headers, integrity, isHistoryNavigation, keepalive, method, mode, redirect, referrer, referrerPolicy, targetAddressSpace, url] = data,
-					response = await serverDriver.handler(new Request(url, { body, cache, credentials, headers, integrity, keepalive, method, redirect, referrer, referrerPolicy })),
-					{ status, statusText } = response
-				;
-
-				const responseBody = await response.arrayBuffer();
-
-				(source as MessagePort).postMessage({ code: "RESPONSE", id, data: [responseBody, status, statusText, Object.fromEntries(response.headers.entries())] }, [responseBody])
-
-				break;
 			}
 		}
 	}
-	// await new Promise(r_init => {
+;
 
-	// 	const { port1: signalPort, port2: signalDest } = new MessageChannel();
 
-	// 	const { port1: serverPort, port2: serverDest } = new MessageChannel();
+serverEstablisherPort.onmessage = ({ data: [msgId, serverPortForDocument, serverPortForFrame] }) => establishmentMsgMap[msgId]?.([serverPortForDocument, serverPortForFrame]);
 
-	// })
-
-	return {
-		url: origin,
-		async reload() {
-
-		},
-		async close() {
-
-		}
-	}
-};
+export { serve }
