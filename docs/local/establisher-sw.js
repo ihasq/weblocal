0ccb07e498b10894966d1b6b0aa239acb9022ed7
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-02-09 21:41:18 +0900

    wip

--- docs/local/establisher-sw.js
+++ docs/local/establisher-sw.js
@@ -1,13 +1,17 @@
-const rand = (length = 8, base = 36) => Math.floor(Math.random() * (base ** length - 1)).toString(base).padStart(length, "0");
-const tEnc = new TextEncoder();
-const serverIdMap = {};
+const
+	rand = (length = 8, base = 36) => Math.floor(Math.random() * (base ** length - 1)).toString(base).padStart(length, "0"),
+	tEnc = new TextEncoder(),
+	serverIdMap = {}
+;
 
 self.onmessage = async ({ data: { code, data }, source }) => {
-	const pubKey = await crypto.subtle.importKey("raw", tEnc.encode(decodeURI(data.pub)), { name: "ECDSA", namedCurve: "P-521" }, false, ["verify"])
 	let serverIdBuf;
 	while((serverIdBuf = rand()) in serverIdMap){};
-	const serverIdComponents = serverIdMap[serverIdBuf] = {};
-	const { port1: serverEstablisherPort, port2: serverEstablisherDest } = new MessageChannel();
+	const
+		pubKey = await crypto.subtle.importKey("raw", tEnc.encode(decodeURI(data.pub)), { name: "ECDSA", namedCurve: "P-521" }, false, ["verify"]),
+		serverIdComponents = serverIdMap[serverIdBuf] = {},
+		{ port1: serverEstablisherPort, port2: serverEstablisherDest } = new MessageChannel()
+	;
 	serverEstablisherPort.onmessage = async ({ data: [msgId, address, encodedAddress, signature] }) => {
 		const
 			[name, rand, serverId] = address.split("-"),
